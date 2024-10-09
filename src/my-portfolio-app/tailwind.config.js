027e02690349d1035d694228d2fba710e4f2e44e
sugar2456 <sugar2456_0526@yahoo.co.jp>
2024-10-10 04:10:36 +0900

    不要な設定を削除

--- src/my-portfolio-app/tailwind.config.js
+++ src/my-portfolio-app/tailwind.config.js
@@ -5,15 +5,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      // keyframes: {
-      //   slideUp: {
-      //     '0%': { transform: 'translateY(100%)', opacity: 0 },
-      //     '100%': { transform: 'translateY(0)', opacity: 1 },
-      //   },
-      // },
-      // animation: {
-      //   slideUp: 'slideUp 0.5s ease-out',
-      // },
       transitionProperty: {
         'transform': 'transform',
         'opacity': 'opacity',
