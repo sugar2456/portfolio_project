1e84d1741dc6fa2abf794ea5d06b23080daca0fb
sugar2456 <sugar2456_0526@yahoo.co.jp>
2024-10-17 08:14:36 +0900

    サイトについてスマホ解像度に対応

--- src/my-portfolio-app/src/pages/sections/AboutSite.js
+++ src/my-portfolio-app/src/pages/sections/AboutSite.js
@@ -3,9 +3,9 @@ function AboutSite() {
     <section id="about-site" className="flex items-center justify-center h-screen bg-gray-100">
       <div className="px-4 text-center">
         <h1 className="mb-10 text-3xl font-bold md:text-6xl">このWebサイトについて</h1>
-        <p className="leading-relaxed">このWebサイトは私のスキルセットや仕事についての考え、ポートフォリオを紹介するサイトになります</p>
-        <p className="leading-relaxed">新卒から現在（2024年度10月）までの経歴を紹介します。</p>
-        <p className="leading-relaxed">少しでも私のスキル感や仕事に関する姿勢にご理解いただけたら幸いです。</p>
+        <p className="text-xs leading-relaxed sm:text-base">このWebサイトは私のスキルセットや仕事についての考え、ポートフォリオを紹介するサイトになります</p>
+        <p className="text-xs leading-relaxed sm:text-base">新卒から現在（2024年度10月）までの経歴を紹介します。</p>
+        <p className="text-xs leading-relaxed sm:text-base">少しでも私のスキル感や仕事に関する姿勢にご理解いただけたら幸いです。</p>
       </div>
     </section>
   )
