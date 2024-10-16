26eac4d971ff64f495a46b61ce6ce2bff9e19ba2
sugar2456 <sugar2456_0526@yahoo.co.jp>
2024-10-17 07:40:18 +0900

    ヘッダーのみスマホ解像度に対応

--- src/my-portfolio-app/src/pages/Header.js
+++ src/my-portfolio-app/src/pages/Header.js
@@ -2,32 +2,32 @@ import { Link } from 'react-scroll';
 function Header() {
   return (
     <header
-      className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-16 bg-white shadow-md"
+      className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-auto bg-white shadow-md"
     >
       <nav>
-        <ul className="flex space-x-8">
+        <ul className="flex md:space-x-8">
           <li className="nav-item">
-            <Link to="about-site" smooth={true} duration={500} className="cursor-pointer">
+            <Link to="about-site" smooth={true} duration={500} className="nav-item-text-class">
               このサイトについて
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="profile" smooth={true} duration={500} className="cursor-pointer">
+            <Link to="profile" smooth={true} duration={500} className="nav-item-text-class">
               私の経歴について
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="skill" smooth={true} duration={500} className="cursor-pointer">
+            <Link to="skill" smooth={true} duration={500} className="nav-item-text-class">
               経験スキルについて
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="thinkingForJob" smooth={true} duration={500} className="cursor-pointer">
+            <Link to="thinkingForJob" smooth={true} duration={500} className="nav-item-text-class">
               仕事についての考え
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="portfolio" smooth={true} duration={500} className="cursor-pointer">
+            <Link to="portfolio" smooth={true} duration={500} className="nav-item-text-class">
               ポートフォリオの紹介
             </Link>
           </li>
