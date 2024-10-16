974c509f7a4ac8da1b6ed2ab30c1583440b9d92c
sugar2456 <sugar2456_0526@yahoo.co.jp>
2024-10-17 08:02:17 +0900

    ヘッダーの追加レイアウト調整

--- src/my-portfolio-app/src/pages/sections/Banner.js
+++ src/my-portfolio-app/src/pages/sections/Banner.js
@@ -3,7 +3,7 @@ function Banner() {
         <div className="flex items-center justify-center text-gray-200 bg-center bg-no-repeat h-80"
             style={{backgroundImage: "url('images/banner.jpg')"}}
         >
-            <h1 className="text-6xl text-center text-outline">OGAWA SATORUのポートフォリオサイト</h1>
+            <h1 className="text-3xl text-center md:text-6xl text-outline">OGAWA SATORUの<br/>ポートフォリオサイト</h1>
         </div>
     );
 }
